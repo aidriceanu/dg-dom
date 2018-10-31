75f53aeeb6915f65107b1d9f23050114de5c0b01
Andrei Idriceanu <andrei.idriceanu@gmail.com>
2018-10-31 23:18:00 +0200

    + preparing for public release

--- index.js
+++ index.js
@@ -4,25 +4,25 @@
 
 if (!Function.prototype.bind) {
     Function.prototype.bind = function (oThis) {
-    if (typeof this !== "function") {
-      // closest thing possible to the ECMAScript 5 internal IsCallable function
-      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-    }
-    
-    var aArgs = Array.prototype.slice.call(arguments, 1), 
-        fToBind = this, 
-        fNOP = function () {},
-        fBound = function () {
-          return fToBind.apply(this instanceof fNOP
-                             ? this
-                             : oThis || window,
-                           aArgs.concat(Array.prototype.slice.call(arguments)));
-        };
+        if (typeof this !== "function") {
+          // closest thing possible to the ECMAScript 5 internal IsCallable function
+          throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+        }
         
-    fNOP.prototype = this.prototype;
-    fBound.prototype = new fNOP();
+        var aArgs = Array.prototype.slice.call(arguments, 1), 
+            fToBind = this, 
+            fNOP = function () {},
+            fBound = function () {
+              return fToBind.apply(this instanceof fNOP
+                                 ? this
+                                 : oThis || window,
+                               aArgs.concat(Array.prototype.slice.call(arguments)));
+            };
+            
+        fNOP.prototype = this.prototype;
+        fBound.prototype = new fNOP();
 
-    return fBound;
+        return fBound;
     };
 }
 
@@ -161,3 +161,20 @@ function dgv2 (html) {
     }
     return [];
 }
+
+function dgv2String (html) {
+    if (html instanceof Function) {
+        return dgv2(html());
+    }
+    
+    if (html instanceof String || typeof html == 'string' || typeof html == 'number') {
+        return html;
+    }
+
+    var generatedHtml = "";
+}
+
+
+exports.render = dgv2;
+
+exports.renderString = dgv2String;
---
+++ test/test.js
@@ -0,0 +1,8 @@
+var expect = require("chai").expect;
+var dg = require("../index.js");
+
+describe("render()", function() {
+    it("test testing", function() {
+        expect(1).to.be.equal(1);
+    });
+});
